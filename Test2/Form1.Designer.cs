e4c4621f21cf07dc9112f31c2d32d7d4ca0619d6
yoonyeop.nam <nyoonyeop@gmail.com>
2024-11-12 21:34:43 +0900

    new

---
+++ Test2/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mybutton1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textModButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.undobutton = new System.Windows.Forms.Button();
+            this.CompareButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mybutton1
+            // 
+            this.mybutton1.Location = new System.Drawing.Point(3, 11);
+            this.mybutton1.Name = "mybutton1";
+            this.mybutton1.Size = new System.Drawing.Size(158, 31);
+            this.mybutton1.TabIndex = 0;
+            this.mybutton1.Text = "경로열기";
+            this.mybutton1.UseVisualStyleBackColor = true;
+            this.mybutton1.Click += new System.EventHandler(this.mybutton1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 48);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(690, 404);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // textModButton
+            // 
+            this.textModButton.Location = new System.Drawing.Point(170, 11);
+            this.textModButton.Name = "textModButton";
+            this.textModButton.Size = new System.Drawing.Size(280, 31);
+            this.textModButton.TabIndex = 2;
+            this.textModButton.Text = "입력한text추가";
+            this.textModButton.UseVisualStyleBackColor = true;
+            this.textModButton.Click += new System.EventHandler(this.textModbutton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(744, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(477, 440);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(741, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "추가내용입력";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // undobutton
+            // 
+            this.undobutton.Location = new System.Drawing.Point(621, 11);
+            this.undobutton.Name = "undobutton";
+            this.undobutton.Size = new System.Drawing.Size(75, 31);
+            this.undobutton.TabIndex = 5;
+            this.undobutton.Text = "Undo";
+            this.undobutton.UseVisualStyleBackColor = true;
+            this.undobutton.Click += new System.EventHandler(this.undobutton_Click);
+            // 
+            // CompareButton
+            // 
+            this.CompareButton.Location = new System.Drawing.Point(456, 11);
+            this.CompareButton.Name = "CompareButton";
+            this.CompareButton.Size = new System.Drawing.Size(159, 31);
+            this.CompareButton.TabIndex = 6;
+            this.CompareButton.Text = "Compare";
+            this.CompareButton.UseVisualStyleBackColor = true;
+            this.CompareButton.Click += new System.EventHandler(this.CompareButton_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Location = new System.Drawing.Point(4, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1244, 482);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.mybutton1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.CompareButton);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.textModButton);
+            this.tabPage1.Controls.Add(this.checkedListBox1);
+            this.tabPage1.Controls.Add(this.undobutton);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1236, 453);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1236, 453);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1236, 453);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(1236, 453);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(1236, 453);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 25);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(1236, 453);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "tabPage6";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 25);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(1236, 453);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "tabPage7";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1260, 529);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button mybutton1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button textModButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button undobutton;
+        private System.Windows.Forms.Button CompareButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+    }
+}
+
---
+++ Test2/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test2
+{
+    public partial class Form1 : Form
+    {
+        private string backupFolderPath; // 백업 폴더의 경로
+        private string selectedPath; // 선택한 폴더의 경로
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void mybutton1_Click(object sender, EventArgs e)
+        {
+            // 폴더 선택 대화 상자 생성
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedPath = folderBrowserDialog.SelectedPath;
+
+                    // 선택된 폴더의 상위 폴더에 "backup" 폴더 경로 설정
+                    string parentDirectory = Directory.GetParent(selectedPath).FullName;
+                    backupFolderPath = Path.Combine(parentDirectory, "backup");
+                    MessageBox.Show(backupFolderPath); //debug용
+
+                    // backup 폴더가 없으면 생성
+                    if (!Directory.Exists(backupFolderPath))
+                    {
+                        Directory.CreateDirectory(backupFolderPath);
+                    }
+
+                    // 선택한 폴더의 모든 .txt 및 .dcm 파일 절대 경로를 가져와 CheckedListBox에 추가
+                    string[] txtFiles = Directory.GetFiles(selectedPath, "*.txt", SearchOption.AllDirectories);
+                    string[] dcmFiles = Directory.GetFiles(selectedPath, "*.dcm", SearchOption.AllDirectories);
+
+                    // CheckedListBox에 절대 경로 추가
+                    checkedListBox1.Items.Clear();
+                    foreach (string filePath in txtFiles.Concat(dcmFiles))
+                    {
+                        // 절대 경로를 그대로 추가
+                        checkedListBox1.Items.Add(filePath);
+                    }
+
+                }
+            }
+        }
+
+        private void BackupFiles()
+        {
+            // 각 파일을 백업 폴더에 복사하고 동일한 폴더 구조 생성
+            foreach (var selectedItem in checkedListBox1.CheckedItems)
+            {
+                string originalFilePath = selectedItem.ToString();
+                // 상대 경로 생성
+                Uri baseUri = new Uri(Directory.GetParent(backupFolderPath).FullName + Path.DirectorySeparatorChar);
+                
+                Uri fileUri = new Uri(originalFilePath);
+                string relativePath = Uri.UnescapeDataString(baseUri.MakeRelativeUri(fileUri).ToString()).Replace('/', Path.DirectorySeparatorChar);
+
+
+                // 첫 번째 하위 폴더 제거
+                string[] pathParts = relativePath.Split(Path.DirectorySeparatorChar);
+                if (pathParts.Length > 1)
+                {
+                    relativePath = Path.Combine(pathParts.Skip(1).ToArray()); // 첫 번째 폴더를 제거하고 경로 다시 결합
+                }
+
+                string backupFilePath = Path.Combine(backupFolderPath, relativePath);
+
+                // 백업 폴더 내 파일이 위치한 디렉터리 생성
+                string backupDir = Path.GetDirectoryName(backupFilePath);
+                if (!Directory.Exists(backupDir))
+                {
+                    Directory.CreateDirectory(backupDir);
+                }
+
+                // 파일을 백업 폴더에 복사
+                File.Copy(originalFilePath, backupFilePath, true);
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textModbutton_Click(object sender, EventArgs e)
+        {
+            BackupFiles(); // 파일 변경 전에 백업 생성
+            string textToAdd = textBox1.Text;
+
+            foreach (var selectedItem in checkedListBox1.CheckedItems)
+            {
+                string filePath = selectedItem.ToString();
+                try
+                {
+                    // 파일 끝에 한 줄 내린 후 textBox1에 입력된 텍스트 추가
+                    File.AppendAllText(filePath, Environment.NewLine + textToAdd);
+                    MessageBox.Show("선택된 파일에 텍스트가 추가되었습니다.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"파일을 수정하는 중 오류 발생: {ex.Message}");
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void undobutton_Click(object sender, EventArgs e)
+        {
+            // 백업된 파일을 원래 파일로 복구
+            foreach (var selectedItem in checkedListBox1.CheckedItems)
+            {
+                string originalFilePath = selectedItem.ToString();
+
+                // 백업 파일 경로 생성
+                Uri baseUri = new Uri(Directory.GetParent(backupFolderPath).FullName + Path.DirectorySeparatorChar);
+                Uri fileUri = new Uri(originalFilePath);
+                string relativePath = Uri.UnescapeDataString(baseUri.MakeRelativeUri(fileUri).ToString()).Replace('/', Path.DirectorySeparatorChar);
+
+                string backupFilePath = Path.Combine(backupFolderPath, relativePath);
+
+                try
+                {
+                    if (File.Exists(backupFilePath))
+                    {
+                        File.Copy(backupFilePath, originalFilePath, true); // 백업 파일을 원래 위치에 덮어쓰기
+                        MessageBox.Show("backupFilePath"+ backupFilePath+"\roriginalFilePaht"+ backupFilePath);
+                        MessageBox.Show("선택된 파일이 원상복구되었습니다.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"파일을 복구하는 중 오류 발생: {ex.Message}");
+                }
+            }
+            MessageBox.Show("선택된 파일이 원상복구되었습니다.");
+        }
+
+        private void CompareButton_Click(object sender, EventArgs e)
+        {
+            string winMergePath = @"C:\Program Files\WinMerge\WinMergeU.exe"; // WinMerge 실행 파일 경로
+
+            if (!File.Exists(winMergePath))
+            {
+                MessageBox.Show("WinMerge가 설치되어 있지 않거나 경로가 잘못되었습니다.");
+                return;
+            }
+
+            if (!Directory.Exists(backupFolderPath) || string.IsNullOrEmpty(selectedPath))
+            {
+                MessageBox.Show("백업 폴더 또는 원본 폴더 경로가 잘못되었습니다.");
+                return;
+            }
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = winMergePath, // 실행할 프로그램의 경로
+                Arguments = $"\"{backupFolderPath}\" \"{selectedPath}\"", // 명령줄 인수, 두 경로를 전달
+                UseShellExecute = false // 프로세스를 쉘 없이 직접 실행
+            });
+        }
+    }
+}
